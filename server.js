cba650e03d72348a965b36235361b3b637a64874
Fernando <fnoriduki@gmail.com>
2021-04-09 16:47:58 -0300

    alterar configs

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
-
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
-app.use(requireHTTPS);
+//Serve only the static files form the dist directory
 app.use(express.static('./dist/diet-center'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/diet-center/'}),
+    res.sendFile('index.html', {root: 'dist/diet-center'}),
 );
 
+//Start the app by Listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
